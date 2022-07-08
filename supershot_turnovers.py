ecf6d96c831a3c84aeb3e8693e29a867864cf8f6
Shari <104345735+shariholden@users.noreply.github.com>
2022-07-08 21:39:00 +1000

    Update supershot_turnovers.py

--- supershot_turnovers.py
+++ supershot_turnovers.py
@@ -1,35 +1,48 @@
 import json
 from matplotlib import pyplot as plt
+import numpy as np
+
 
 def dictionary_creator(key_list):
-    dict={}
-    for i in key_list:
-        dict[i]=[]
-    return dict
+    dictt = {}
+    for k in key_list:
+        dictt[k] = []
+    return dictt
+
 
 def ss_turnover(filename, dict1, dict2):
     f = open(filename)
     datastore = json.load(f)
-    scoreflow=datastore['matchStats']['scoreFlow']['score']
-    teams=datastore['matchStats']['teamInfo']['team']
-    team1id=teams[0]['squadId']
-    team1name=teams[0]['squadName']
-    team1supershots=datastore['matchStats']['teamStats']['team'][0]['goal_from_zone2']
-    team2id=teams[1]['squadId']
+    scoreflow = datastore['matchStats']['scoreFlow']['score']
+    teams = datastore['matchStats']['teamInfo']['team']
+    team1id = teams[0]['squadId']
+    team1name = teams[0]['squadName']
+    if datastore['matchStats']['teamStats']['team'][0]['squadId'] == team1id:
+        team1supershots = datastore['matchStats']['teamStats']['team'][0]['goal_from_zone2']
+        team2supershots = datastore['matchStats']['teamStats']['team'][1]['goal_from_zone2']
+    else:
+        team1supershots = datastore['matchStats']['teamStats']['team'][1]['goal_from_zone2']
+        team2supershots = datastore['matchStats']['teamStats']['team'][0]['goal_from_zone2']
+    team2id = teams[1]['squadId']
     team2name = teams[1]['squadName']
-    team2supershots = datastore['matchStats']['teamStats']['team'][1]['goal_from_zone2']
-    team1turnover, team2turnover=0, 0
-    for i in range(len(scoreflow)):
-        if scoreflow[i]['scoreName']=="2pt Miss":
-            if i+1!=len(scoreflow) and scoreflow[i+1]['period']==scoreflow[i]['period']:
-                if scoreflow[i]['squadId']==team1id and scoreflow[i+1]['squadId']==team2id:
-                    team1turnover+=1
-                elif scoreflow[i]['squadId']==team2id and scoreflow[i+1]['squadId']==team1id:
-                    team2turnover+=1
-    team1ratio=team1supershots/(team1turnover+team1supershots)
-    team2ratio = team2supershots / (team2turnover+team2supershots)
+    team1turnover, team2turnover = 0, 0
+    for k in range(len(scoreflow)):
+        if scoreflow[k]['scoreName'] == "2pt Miss":
+            if k + 1 != len(scoreflow) and scoreflow[k + 1]['period'] == scoreflow[k]['period']:
+                if scoreflow[k]['squadId'] == team1id and scoreflow[k + 1]['squadId'] == team2id:
+                    team1turnover += 1
+                elif scoreflow[k]['squadId'] == team2id and scoreflow[k + 1]['squadId'] == team1id:
+                    team2turnover += 1
+    try:
+        team1ratio = team1supershots / (team1turnover + team1supershots)
+    except ZeroDivisionError:
+        team1ratio = np.nan
+    try:
+        team2ratio = team2supershots / (team2turnover + team2supershots)
+    except ZeroDivisionError:
+        team2ratio = np.nan
 
-    #create dictionary
+    # create dictionary
     dict1[team1name].append(team1ratio)
     dict1[team2name].append(team2ratio)
     dict2[team1name].append(team2ratio)
@@ -37,17 +50,20 @@ def ss_turnover(filename, dict1, dict2):
 
     return dict1, dict2
 
+
 if __name__ == '__main__':
-    team_list=['Adelaide Thunderbirds', 'Collingwood Magpies', 'GIANTS Netball', 'Melbourne Vixens', 'NSW Swifts', 'Queensland Firebirds', 'Sunshine Coast Lightning', 'West Coast Fever']
-    ratio_for, ratio_against=dictionary_creator(team_list), dictionary_creator(team_list)
+    team_list = ['Adelaide Thunderbirds', 'Collingwood Magpies', 'GIANTS Netball', 'Melbourne Vixens', 'NSW Swifts',
+                 'Queensland Firebirds', 'Sunshine Coast Lightning', 'West Coast Fever']
+    ratio_for, ratio_against = dictionary_creator(team_list), dictionary_creator(team_list)
     for i in range(1, 10):
         for j in range(1, 5):
-            ratio_for, ratio_against=ss_turnover('116650'+str(i)+ '0'+str(j)+'.json', ratio_for, ratio_against)
+            ratio_for, ratio_against = ss_turnover('116650' + str(i) + '0' + str(j) + '.json', ratio_for, ratio_against)
     for i in range(10, 15):
         for j in range(1, 5):
-            ratio_for, ratio_against=ss_turnover('11665'+str(i)+ '0'+str(j)+'.json', ratio_for, ratio_against)
+            ratio_for, ratio_against = ss_turnover('11665' + str(i) + '0' + str(j) + '.json', ratio_for, ratio_against)
+    print(ratio_for, ratio_against)
 
-    x=list(range(1, 15))
+    x = list(range(1, 15))
 
     ax = plt.axes()
     ax.set_facecolor('cornsilk')
@@ -62,7 +78,7 @@ if __name__ == '__main__':
     plt.plot(x, ratio_for['Queensland Firebirds'], label='Queensland Firebirds', color='purple')
     plt.plot(x, ratio_for['Sunshine Coast Lightning'], label='Sunshine Coast Lightning', color='gold')
     plt.plot(x, ratio_for['West Coast Fever'], label='West Coast Fever', color='limegreen')
-    plt.plot(x, [0.5]*14, color='lightgrey', linestyle='dashed')
+    plt.plot(x, [0.5] * 14, color='lightgrey', linestyle='dashed')
     plt.grid()
     plt.legend()
     plt.show()
@@ -85,13 +101,13 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
-    new_dict_for, new_dict_against={}, {}
+    new_dict_for, new_dict_against = {}, {}
     for i in team_list:
-        new_dict_for[i]=sum(ratio_for[i])/14
-        new_dict_against[i]=sum(ratio_against[i])/14
-    average_for=0
+        new_dict_for[i] = np.nanmean(ratio_for[i])
+        new_dict_against[i] = np.nanmean(ratio_against[i])
+    average_for = 0
     for i in team_list:
-        average_for+=new_dict_for[i]
+        average_for += new_dict_for[i]
 
     ax = plt.axes()
     ax.set_facecolor('cornsilk')
@@ -107,7 +123,8 @@ if __name__ == '__main__':
     plt.bar('Queensland\nFirebirds', new_dict_for['Queensland Firebirds'], color='purple')
     plt.bar('Sunshine Coast\nLightning', new_dict_for['Sunshine Coast Lightning'], color='gold')
     plt.bar('West Coast\nFever', new_dict_for['West Coast Fever'], color='limegreen')
-    plt.plot(list(range(-1, 9)), [average_for/8]*10, color='lightgrey', linestyle='dashed', label='Average supershots scored vs turnovers from supershot attempts')
+    plt.plot(list(range(-1, 9)), [average_for / 8] * 10, color='lightgrey', linestyle='dashed',
+             label='Average supershots scored vs turnovers from supershot attempts')
     plt.legend()
     plt.show()
 
